b39c83430e62340f15e535a7def30ed7de12361f
rmgarcia <rockdrigo.mtz@gmail.com>
2018-05-09 20:24:24 -0500

    Agregando spec de Order

---
+++ spec/order_spec.rb
@@ -0,0 +1,20 @@
+require 'order'
+require 'line_item'
+require 'item'
+require 'money'
+
+RSpec.describe Order do
+  it "sums the prices of its line items" do
+    order = Order.new
+
+    order.add_entry(LineItem.new(:item => Item.new(
+      :price => Money.new(1.11, :USD)
+    )))
+    order.add_entry(LineItem.new(:item => Item.new(
+      :price => Money.new(2.22, :USD),
+      :quantity => 2
+    )))
+
+    expect(order.total).to eq(Money.new(5.55, :USD))
+  end
+end
