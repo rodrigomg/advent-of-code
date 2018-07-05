4427be594b0f1cbcb7e0310f476a7dd4dd1e72b7
Rodrigo Martinez Garcia <rockdrigo.mtz@gmail.com>
2018-07-04 20:26:17 -0500

    OD

--- Day2.groovy
+++ Day2.groovy
@@ -1,14 +1,16 @@
-def input = """29x13x26
-11x11x14"""
-
+def input = new File("input_day2.txt").text
+total = []
 input.split("[\n]").each{line ->
  //2*l*w + 2*w*h + 2*h*l
  //l=0, w=1, h=2
  String[] data = line.split("[x]")
- println data[0]
- println data[1]
- println data[2]
- println "1: ${2*(data[0].toInteger() * data[1].toInteger())}"
- println "2: ${2*(data[1].toInteger() * data[2].toInteger())}"
- println "3: ${2*(data[2].toInteger() * data[0].toInteger())}"
+ l = data[0].toInteger()
+ w = data[1].toInteger()
+ h = data[2].toInteger()
+ sideA = (2*l*w)
+ sideB = (2*w*h)
+ sideC = (2*h*l)
+ minSide = [l*w,w*h,h*l].min()
+ total.add(sideA+sideB+sideC+minSide)
 }
+println total.sum()
