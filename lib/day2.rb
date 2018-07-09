4bf9b21c66039f47e2e1e50f1137a7b16417537f
rockdrigo <rockdrigo.mtz@gmail.com>
2018-07-08 21:58:41 -0500

    vamo avando :relieved:  leyendo input

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,16 +1,5 @@
-def input = new File("input_day2.txt").text
-total = []
-input.split("[\n]").each{line ->
- //2*l*w + 2*w*h + 2*h*l
- //l=0, w=1, h=2
- String[] data = line.split("[x]")
- l = data[0].toInteger()
- w = data[1].toInteger()
- h = data[2].toInteger()
- sideA = (2*l*w)
- sideB = (2*w*h)
- sideC = (2*h*l)
- minSide = [l*w,w*h,h*l].min()
- total.add(sideA+sideB+sideC+minSide)
-}
-println total.sum()
+f = File.open("/Users/technosoul/development/advent-of-code/input_day2.txt", "r")
+f.each_line do |line|
+  puts line
+end
+f.close
