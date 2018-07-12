ad39730a2b5dc1d440a418d1d7e9d8422272a15c
technosoul <rockdrigo.mtz@gmail.com>
2018-07-11 23:38:54 -0500

    adding interpolation variables and test split in ruby :see_no_evil:

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,7 +1,8 @@
-f = File.open(Dir.pwd+"/input_day2.txt", "r")
+f = File.open("#{Dir.pwd}/input_day2.txt", "r")
 array = []
 f.each_line do |line|
-  puts line
+  puts line.split("x").size()
+	puts line
 end
 f.close
 puts array.size()
