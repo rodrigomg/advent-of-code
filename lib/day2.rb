9623334bb6c0522bd0e1a5fc7ce50725684b0057
technosoul <rockdrigo.mtz@gmail.com>
2018-07-14 19:11:56 -0500

    resolviendo día 2 con ruby :tada:

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,8 +1,15 @@
 f = File.open("#{Dir.pwd}/input_day2.txt", "r")
-array = []
+total = []
 f.each_line do |line|
-  puts line.split("x").size()
-	puts line
+  data =  line.split("x")
+	l = Integer(data[0])
+	w = Integer(data[1])
+	h = Integer(data[2])
+	sideA = (2 * l * w)
+	sideB = (2 * w * h)
+	sideC = (2 * h * l)
+	minSide = [l*w, w*h, h*l ].min
+	total << (sideA + sideB+ sideC + minSide)
 end
 f.close
-puts array.size()
+puts total.inject(0){|sum,x| sum + x }
